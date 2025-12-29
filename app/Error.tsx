20b84ea1d031d773e716c5309bebf36ae9cfe6f6
Isha <isha@gmail.com>
2025-12-29 18:09:08 +0000

    Added error page for error handling

---
+++ app/Error.tsx
@@ -0,0 +1,13 @@
+"use client"
+export default function Error({error, reset}: {error: Error, reset:()=>void})
+{
+    return(<>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold text-red-600">Something went wrong!</h1>
+      <p>{error.message}</p>
+      <button onClick={reset} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+        Try Again
+      </button>
+    </div>
+    </>)
+}
