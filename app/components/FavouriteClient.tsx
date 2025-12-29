4c7ae3672be47600dec66a1f153d9f8404db5a49
Isha <isha@gmail.com>
2025-12-29 18:11:15 +0000

    its for saving the favourites in local storage

---
+++ app/components/FavouriteClient.tsx
@@ -0,0 +1,109 @@
+"use client"
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
+import Image from "next/image"
+import { useEffect, useState } from "react";
+import FavouriteHeart from './FavouriteHeart'
+import Link from "next/link"
+
+
+type Product = {
+  _id: number,
+  title: string,
+  description: string,
+  image: string,
+  price: number,
+  stock: number,
+  brand: string,
+  category: string,
+  isNew: boolean,
+  size: string[],
+  rating: number,
+  oldPrice: string,
+  discountedPrice: number
+
+}
+
+export default function FavouriteClient()
+{
+    
+    
+   
+    const [ products, setProducts] = useState<number[]>([])
+
+
+    useEffect(()=>{
+        const fetchProducts = async()=>{
+            try{
+            const stored:number[] = JSON.parse(
+            localStorage.getItem('favourites') || "[]"
+        );
+        if(stored.length === 0) return;
+        const productsData = await Promise.all(
+            stored.map(async(id)=>{
+                 const res = await fetch(`https://fakestoreapiserver.reactbd.org/api/products/${id}`)
+                 return res.json();
+
+            })
+        );
+        setProducts(productsData)
+        
+        }
+        catch(error)
+        {
+            console.error('Failed to fetch products',error)
+        }
+        };
+        fetchProducts();
+      
+    },[]);
+
+    return(<>
+    <div>
+        {
+            products.length == 0 ? (
+                <p> No favourites yet </p>
+            ):(
+               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
+      
+                    {products.map((product)=>(
+                         <Link key={product._id} href={`Home/${product._id}`}>
+                       <Card className="group" key={product._id}>
+
+            <CardContent>
+              <div className="relative w-full h-64 flex justify-center ">
+                <Image
+                  src={product.image}
+                  alt={product.title}
+                  fill
+                  className="object-cover rounded"
+                />
+                <div className="absolute  bottom-0 text-center gap-1 bg-black w-full opacity-100 transition-opacity group-hover:opacity-0 ">
+                  {Array.from({ length: 5 }, (_, i) => (
+                    <span key={i} className="text-yellow-500">
+                      {i < Math.floor(product.rating) ? "⭐" : "☆"}
+                    </span>
+                  ))}
+                  <span className="ml-2 text-gray-500 text-sm">{product.rating.toFixed(1)}</span>
+                </div>
+              
+                <button className=" absolute bottom-2 text-black bg-white opacity-0 group-hover:opacity-100 transition-opacity  transition rounded-lg p-2">  <FavouriteHeart _id={product._id}/> Add to wishlist</button>
+              </div>
+              
+              
+            </CardContent>
+            <CardHeader>
+              <CardTitle>{product.title} </CardTitle>
+              <span className="text-gray-500 text-lg"> {product.brand}  {product.isNew && <Badge variant="outline">New</Badge>}</span>
+             
+              <p className="mt-2 text-lg font-semibold">${product.price}  <span className="text-gray-500 line-through text-sm ml-2">${product.oldPrice}</span> <span className="text-sm text-orange-600">({product.discountedPrice/10}%)</span></p>
+            </CardHeader>
+          </Card>
+          </Link>
+                    ))}
+                </div>
+            )
+        }
+    </div>
+    </>)
+}
