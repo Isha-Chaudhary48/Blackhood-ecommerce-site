6e21c6303672eea6d79b9a4bf230fc884002f03e
Isha <isha@gmail.com>
2026-01-27 14:13:23 +0000

    Here checking of signature has beed done if they are same that means success then order saved in the db

---
+++ app/api/payment/verify/route.ts
@@ -0,0 +1,85 @@
+import pool from '@/lib/db';
+import crypto from 'crypto';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+    try {
+        const {
+            razorpay_order_id,
+            razorpay_payment_id,
+            razorpay_signature,
+            orderId,
+        } = await req.json();
+
+        const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+        const expectedSignature = crypto.createHmac('sha256', process.env.RAZORPAY_TEST_KEY_SECRET!).update(body).digest('hex')
+        console.log(expectedSignature)
+
+        if (expectedSignature !== razorpay_signature) {
+            return NextResponse.json({
+                success: false,
+                message: "Payment signature verification failed"
+            },
+                {
+                    status: 400
+                })
+        }
+
+        const orderRes = await pool.query(
+            `SELECT user_id FROM orders WHERE id= $1`, [orderId]
+
+        );
+        const userId = orderRes.rows[0].user_id;
+
+        const cartRes = await pool.query(
+            `SELECT id from carts WHERE user_id = $1`, [userId]
+        )
+
+        if (cartRes.rowCount === 0) {
+            return NextResponse.json({
+                success: false,
+                message: "No cart items found"
+            })
+        }
+        const cartId = cartRes.rows[0].id;
+
+        const cartItems = await pool.query(
+            `SELECT product_name,price,quantity,size,image,product_id FROM cart_items WHERE cart_id = $1 `, [cartId]
+        )
+
+        for (const item of cartItems.rows) {
+            await pool.query(
+                `INSERT INTO order_items (order_id,product_id,quantity,price,size,product_image,product_name) VALUES($1,$2,$3,$4,$5,$6,$7)`, [orderId, item.product_id, item.quantity, item.price, item.size, item.image, item.product_name]
+            )
+        }
+
+        await pool.query(
+            `DELETE FROM cart_items WHERE cart_id=$1`, [cartId]
+        );
+
+        await pool.query(
+            `UPDATE orders 
+             SET payment_status = 'PAID',
+             status='DONE',
+            razorpay_order_id= $1,
+            razorpay_payment_id=$2
+            WHERE id = $3`, [razorpay_order_id, razorpay_payment_id, orderId]
+        );
+        return NextResponse.json({
+            success: true,
+            message: "payment verified and order placed successfully"
+        })
+
+
+    }
+    catch (error) {
+        console.error("Payment verification error", error)
+        return NextResponse.json({
+            success: false,
+            message: "Server error during payment verification",
+
+        }, { status: 500 })
+
+    }
+}
