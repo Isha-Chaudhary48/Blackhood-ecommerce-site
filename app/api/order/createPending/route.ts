0fbdcdc7b89eee134108057d9ed2d4fdc9eda86e
Isha <isha@gmail.com>
2026-01-31 14:26:49 +0000

    To showcase the all ordered item of the user that is singned in

--- app/api/order/createPending/route.ts
+++ app/api/order/createPending/route.ts
@@ -12,6 +12,10 @@ export async function POST(req: Request) {
     try {
 
         const { totalAmount } = await req.json();
+        if (!totalAmount || totalAmount <= 0) {
+            return NextResponse.json({ message: "Invalid total amount" }, { status: 400 });
+        }
+        console.log(totalAmount)
         const user = await getAuthUser();
         const user_id = user?.userId;
         console.log(user_id)
@@ -22,27 +26,26 @@ export async function POST(req: Request) {
 
         }
         const deliveryDate = new Date();
-deliveryDate.setDate(deliveryDate.getDate() + 5);
+        deliveryDate.setDate(deliveryDate.getDate() + 5);
 
         const res = await pool.query(
-            `INSERT INTO orders (user_id,total_amount,payment_status,payment_method,delivery_date,status) VALUES($1,$2,'PENDING','RAZORPAY',$3,'PROCESSING') RETURNING id`, [user_id, totalAmount,deliveryDate]
+            `INSERT INTO orders (user_id,total_amount,payment_status,payment_method,delivery_date,status) VALUES($1,$2,'PENDING','RAZORPAY',$3,'PROCESSING') RETURNING id`, [user_id, totalAmount, deliveryDate]
         )
-        if(!res)
-        {
+        if (!res) {
             console.log("failed")
         }
-        
-        const internalOrderId = res.rows[0].id;
-        console.log(internalOrderId)
+
+        const internalOrderId = res.rows[0].id.toString();
+        console.log("order id", internalOrderId);
 
         const razorpayOrder = await razorpay.orders.create({
             amount: Math.round(totalAmount * 100),
             currency: 'INR',
-            receipt: "receipt_" +  internalOrderId.slice(0, 20)
+            receipt: "receipt_" + internalOrderId.slice(0, 20)
         })
-        if(!razorpayOrder)
-        {
-            console.log(razorpayOrder);
+        console.log("razorpay order", razorpayOrder)
+        if (!razorpayOrder) {
+            console.log("Not found razorpayorder", razorpayOrder);
             return;
         }
 
@@ -60,7 +63,7 @@ deliveryDate.setDate(deliveryDate.getDate() + 5);
 
     }
     catch (error) {
-        console.log("error in create pending",error)
+        console.log("error in create pending", error)
 
         return NextResponse.json(
             {
@@ -72,36 +75,51 @@ deliveryDate.setDate(deliveryDate.getDate() + 5);
 
 
 }
-export async function GET(req:Request)
-{
-    try{
+export async function GET(req: Request) {
+    try {
         const user = await getAuthUser();
         const user_id = user?.userId;
-        if(!user_id)
-        {
+        if (!user_id) {
             return NextResponse.json(
                 {
-                    message:"someting went wrong with authorization"
+                    message: "someting went wrong with authorization"
                 },
-                {status: 401}
+                { status: 401 }
             )
         }
         const ordersRes = await pool.query(
-            `SELECT * FROM orders WHERE user_id = $1 RETURNING id`,[user_id]
-        )  
-        if(ordersRes.rowCount ===0)
-            {
-                return NextResponse.json({
-                    message:"NO ORDER FOUND"
-                })
-            }
-            return NextResponse.json(ordersRes.rows);   
+            `SELECT 
+       o.id AS order_id ,
+       o.order_date,
+       o.delivery_date,
+       o.status ,
+       o.payment_status,
+       
+       oi.id AS order_item_id,
+       oi.product_id,
+       oi.quantity,
+       oi.size,
+       oi.product_image,
+       oi.price,
+       oi.product_name,
+       oi.created_at
+       FROM orders o
+      LEFT JOIN order_items oi ON o.id = oi.order_id
+      WHERE o.status IN ('IN TRANSIT','DELIVERED') AND o.user_id = $1
+       ORDER BY o.created_at DESC
+       
+       `, [user_id]
+        )
+        console.log("orders", ordersRes)
+        if (ordersRes.rowCount === 0) {
+            return NextResponse.json([])
+        }
+        return NextResponse.json(ordersRes.rows);
     }
-    catch(error)
-    {
-        console.log("error to get orders",error);
+    catch (error) {
+        console.log("error to get orders", error);
         return NextResponse.json({
-            message:"server error in getting orders from db"
+            message: "server error in getting orders from db"
         })
     }
 }
