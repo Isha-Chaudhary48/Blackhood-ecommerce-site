bd223027507ba497f462999f17fec34afe4c01e0
Isha <isha@gmail.com>
2026-01-31 14:19:54 +0000

    Made type of cart items

---
+++ app/types/cartItem.ts
@@ -0,0 +1,16 @@
+type TCartItem = {
+    id: string,
+    product_id: number,
+    product_name: string,
+    price: number,
+    quantity: number,
+    size: string,
+    image: string,
+    isNew: boolean,
+    brand: string,
+    discountedPrice: number,
+    oldPrice: string
+
+
+
+}
