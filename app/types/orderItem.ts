0aedae1429235c0f82014879abc6ec3e52ea9eb3
Isha <isha@gmail.com>
2026-01-31 14:19:40 +0000

    made type for order items page

---
+++ app/types/orderItem.ts
@@ -0,0 +1,21 @@
+type TOrderItem = {
+
+    id: string,
+    order_item_id: string,
+    order_id: string,
+    product_id: number,
+    product_name: string,
+    price: number,
+    quantity: number,
+    size: string,
+    product_image: string,
+    order_date: Date,
+    delivery_date: Date,
+    payment_status: string,
+    status: string
+
+
+
+
+
+}
