2e15cccb2e346e3264c903022f554300966cd74f
Isha <isha@gmail.com>
2026-01-31 14:19:14 +0000

    changed font size

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,9 @@
-export default function Footer()
-{
-    return(<>
-    <div className="p-4 md:w-80 bg-zinc-900 text-white text-center space-y-2">
-    <p>© 2026 Blackhood. All rights reserved.</p>
-    <p>Made with ❤️ for smart shoppers.</p>
-    </div>
-   
+export default function Footer() {
+    return (<>
+        <div className=" text-md p-4 md:w-80 bg-zinc-900 text-white text-center space-y-2 ">
+            <p>© 2026 Blackhood. All rights reserved.</p>
+            <p>Made with ❤️ for smart shoppers.</p>
+        </div>
+
     </>)
 }
