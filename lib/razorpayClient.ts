428f98fba3cb27613101eb60034b167f5ba79dcd
Isha <isha@gmail.com>
2026-01-31 14:17:53 +0000

    Added function on sucess to redirect on the orders page when payment successful

--- lib/razorpayClient.ts
+++ lib/razorpayClient.ts
@@ -1,4 +1,6 @@
 
+import toast from "react-hot-toast";
+
 
 export const loadRazorpay = () => {
 
@@ -15,53 +17,59 @@ export const loadRazorpay = () => {
 
         document.body.appendChild(script);
 
-
-
     })
 
 }
 export const openRazorpay = ({
     amount,
-    orderId,
-    razorpay_order_id
+    razorpayOrderId,
+    onSuccess
+
 }: {
-   
+
     amount: number,
-    orderId: string,
-     razorpay_order_id :string,
+    razorpayOrderId: string,
+    onSuccess?: () => void,
 }) => {
-     console.log("Razorpay orderid",orderId)
-     console.log("razor pay id ",  razorpay_order_id)
-    const options={
-        key:process.env.NEXT_PUBLIC_RAZORPAY_API_KEY!,
-        amount :amount * 100,
-        currency:"INR",
-        order_id :   razorpay_order_id,
-       
 
-        handler: async function(response: any){
-            console.log("Payment Success HIII ",response)
+    console.log("razor pay id ", razorpayOrderId
+    )
+    const options = {
+        key: process.env.NEXT_PUBLIC_RAZORPAY_API_KEY!,
+        amount: amount * 100,
+        currency: "INR",
+        order_id: razorpayOrderId
+        ,
 
-            const verifyPayment = await fetch('/api/payment/verify',{
-                method:'POST',
-                headers:{
-                    "Content-Type":"application/json",
+
+        handler: async function (response: any) {
+            console.log("Payment Success HIII ", response)
+
+            const verifyPayment = await fetch('/api/payment/verify', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
                 },
-                body:JSON.stringify({
-                    razorpay_order_id: response.razorpay_order_id,
-                    razorpay_payment_id:response.razorpay_payment_id,
+                credentials: "include",
+                body: JSON.stringify({
+
+                    razorpayOrderId: response.razorpay_order_id,
+                    razorpay_payment_id: response.razorpay_payment_id,
                     razorpay_signature: response.razorpay_signature,
-                    orderId
+
                 })
             })
+
             const data = await verifyPayment.json();
 
-            if(data.success)
-            {
-                alert("Payment verified and order placed");
+            if (data.success) {
+                toast.success("payment successful")
+                if (onSuccess) onSuccess();
+
+
             }
-            else{
-                alert("Payment verification failed");
+            else {
+                toast.error(data.message || "payment verification failed")
             }
 
 
